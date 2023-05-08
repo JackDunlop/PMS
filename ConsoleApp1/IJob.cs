a457e5638a4d566f0cf1a9d142897449f2ec8ab2
JackDunlop <JackDunlops@icloud.com>
2023-05-08 22:26:25 +1000

    added IJob and Jobs

---
+++ ConsoleApp1/IJob.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass3
+{
+    public interface IJob
+    {
+
+        string JobID { get; set; }
+
+        uint JobTime { get; set; }
+
+        Dictionary<string, object> JobDependencies { get; set; }
+
+    }
+
+   
+}
+
---
+++ ConsoleApp1/Job.cs
@@ -0,0 +1,24 @@
+﻿using Ass3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass3
+{
+    public class Job : IJob
+    {
+        public string JobID { get; set; }
+        public uint JobTime { get; set; }
+        public Dictionary<string, object> JobDependencies { get; set; }
+
+        public Job(string jobId, uint jobTime, Dictionary<string, object> jobDependencies)
+        {
+            JobID = jobId;
+            JobTime = jobTime;
+            JobDependencies = jobDependencies;
+        }
+    }
+   
+}
